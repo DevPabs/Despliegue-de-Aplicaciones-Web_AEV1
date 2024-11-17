43eaa1fb705571c0083dec0dd838deb0e93ae604
DevPabs <pacade01@alumnatflorida.es>
2024-11-17 20:48:51 +0100

    Modificado el archivo index.php, creado el archivo estilos.css y creado el archivo victoria.php

---
+++ resultados/victoria.php
@@ -0,0 +1,20 @@
+<?php 
+session_start(); 
+// Reiniciar la sesión para un nuevo juego
+session_destroy(); // Esto destruye la sesión actual
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>¡Ganaste!</title>
+    <link rel="stylesheet" href="../estilos.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Jersey+25+Charted&family=Share+Tech+Mono&family=Tiny5&display=swap">
+</head>
+<body>
+    <h1>¡Enhorabuena!</h1>
+    <p class="p1">¡Has acertado todas las letras y has ganado! La palabra era: <?php echo $_SESSION['palabra']; ?></p>
+    <a href="../" class="button">Jugar de nuevo</a>
+</body>
+</html>
